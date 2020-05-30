d7e3614909648d6ee86f8fe0a7ba362214b3d6d2
ganesh padole <ganeshpadole55@gmail.com>
2020-05-30 19:22:03 +0530

    Added TableView to ViewController.

--- PlainNotes/ViewController.swift
+++ PlainNotes/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    var data = ["Item 1","Item 2","Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteTakingCell")
+        cell?.textLabel?.text = data[indexPath.row]
+        
+        return cell!
+    }
+    
+    
+}
