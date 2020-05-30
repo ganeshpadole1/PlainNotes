e7eefc9cb64ea4b8f71105db483f59e292a5e922
ganesh padole <ganeshpadole55@gmail.com>
2020-05-30 14:25:41 +0530

    Initial Commit

---
+++ PlainNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlainNotes
+//
+//  Created by ganesh padole on 30/05/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
